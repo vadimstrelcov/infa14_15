16874f01320dada3598e4668747bbf91857b1921
vadimstrelcov <vadimstrelcov1996@gmail.com>
2015-04-03 23:31:45 +0300

    add comments for input files

--- Infa15/Heat_equation/main.cpp
+++ Infa15/Heat_equation/main.cpp
@@ -7,22 +7,25 @@
 int main(int argc, char** argv) {
 
 	printf("%d\n",(int)sizeof(Polygon));
+
 	double h_t=0.001;
-	int steps=100;
+	int steps=1000, n_count=1000;
 	/*
-		input1: 60 60
-		input2: 65 55
-		input3: 75 75
-		input4: 100 70
-		input5: 100 100
+		input1:  60  60
+		input2:  65  55
+		input3:  75  75
+		input4: 100  70
+		input5:  10  10
 		input6: 100 100
+		input7:  10  10
+		input8: 120 120
+		input8: 100 100
 	*/
-    Polygon polygon("tests/input5.txt", 1, 20, 20, h_t);
+    Polygon polygon("tests/input1.txt", 1, 100, 100, h_t);
    	printf("%d\n",(int)sizeof(polygon));
     polygon.get_partition();
 
-
-    for (int count=0;count<1000;count++) {
+    for (int count=0;count<n_count;count++) {
 		polygon.solve(count*steps*h_t, steps);
 		printf("%d\n",count);
 	}
